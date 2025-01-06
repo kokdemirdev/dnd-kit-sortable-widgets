014341843cd4a15ea222652c22493dc7bc9067ab
Emin Kokdemir <kokdemirdev@gmail.com>
2025-01-06 06:17:30 +0300

    .

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import { createTheme, CssBaseline, ThemeProvider } from "@mui/material";
 
-createRoot(document.getElementById('root')!).render(
+const theme = createTheme();
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <App />
+    </ThemeProvider>
   </StrictMode>,
-)
+);
